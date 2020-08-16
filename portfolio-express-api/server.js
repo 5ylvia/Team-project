c1b3f0cb72cf38eedf0a320d844d4f2746c53ab7
Sylvia <subin3311@gmail.com>
2020-08-17 09:18:12 +1200

    Basic server setup with endpoints

--- portfolio-express-api/server.js
+++ portfolio-express-api/server.js
@@ -4,8 +4,70 @@ const cors = require("cors");
 
 app.use(cors());
 
+const data = [
+  {
+    id: "sylvia",
+    name: "Sylvia Lee",
+    sources: ["GRAPHIC", "UX/UI", "WEB"],
+    description: "ac velit tellus. Eleifend id suspendisse ipsum etiam at.",
+    skills: [
+      "Graphic Design",
+      "HTML, CSS, JS",
+      "UX/Ui Design",
+      "Adobe Creative Suite",
+    ],
+    contact: {
+      email: "NameEmail@Gmail.com",
+      phone: "021 123 1234",
+      web: "WebDomain.com",
+    },
+  },
+  {
+    id: "thom",
+    name: "Thomas Hendrikz",
+    sources: ["GRAPHIC", "UX/UI", "WEB"],
+    description: "ac velit tellus. Eleifend id suspendisse ipsum etiam at.",
+    skills: [
+      "Graphic Design",
+      "HTML, CSS, JS",
+      "UX/Ui Design",
+      "Adobe Creative Suite",
+    ],
+    contact: {
+      email: "NameEmail@Gmail.com",
+      phone: "021 123 1234",
+      web: "WebDomain.com",
+    },
+  },
+  {
+    id: "vinny",
+    name: "Vincent Pollett",
+    sources: ["GRAPHIC", "UX/UI", "WEB"],
+    description: "ac velit tellus. Eleifend id suspendisse ipsum etiam at.",
+    skills: [
+      "Graphic Design",
+      "HTML, CSS, JS",
+      "UX/Ui Design",
+      "Adobe Creative Suite",
+    ],
+    contact: {
+      email: "NameEmail@Gmail.com",
+      phone: "021 123 1234",
+      web: "WebDomain.com",
+    },
+  },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello world!!");
+});
+
+app.get("/users", (req, res) => {
+  res.send({ users: data });
+});
+
+app.get("/users/:id", (req, res) => {
+  console.log(req.params.id);
 });
 
 app.listen(3000, () => {
